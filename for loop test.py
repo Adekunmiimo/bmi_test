da8eca6d38f515b4490e936191d70cefda0248d7
Leon64-c <cyrussolomon64@gmail.com>
2022-01-30 20:36:04 +0800

    Initial commit

---
+++ for loop test.py
@@ -0,0 +1,33 @@
+a = ["leon", "many", "cyrus"]
+
+for element in a:
+    print(element)
+
+b = [60, 40, 69]
+total = 0
+for e in b:
+    total = total + e
+print(total)
+
+# 1, 2, 3, 4
+
+c = list(range(1, 5))
+print(c)
+
+total2 = 0
+for i in range(1, 5):
+    total2 = + i
+print(total2)
+
+print(list(range(1, 8)))
+print(2 % 3)
+
+total3 = 0
+for i in range (1, 8):
+    if i % 3 == 0:
+        total3 += 1
+print(total3)
+
+# compute all multiples of 3, 5
+# that are less than 100
+print(list(range(1, 100)))
